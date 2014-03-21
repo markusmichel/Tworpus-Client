85e8c94bcac40643662f9abfdd23c3842033c1ac
Markus Michel <markus.michel.1988@googlemail.com>
2014-03-21 15:24:38 +0100

    Force csrf_token generation in main view

--- tworpus/views.py
+++ tworpus/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render
 from django.shortcuts import render_to_response, RequestContext
 import tworpus_user_id
-from django.http import HttpResponse
-
-from session.models import Session
-import json
-
 
 def home(request):
+    request.META["CSRF_COOKIE_USED"] = True
     id = tworpus_user_id.getUid()
     return render_to_response("base.html", {"sid": str(id)}, context_instance=RequestContext(request))
