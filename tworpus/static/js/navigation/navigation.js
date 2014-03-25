e0bfdc2644307fe1df815070f60a9bb8126987db
Markus Michel <markus.michel.1988@googlemail.com>
2014-03-25 22:35:35 +0100

    Mark current navigation item as active

---
+++ tworpus/static/js/navigation/navigation.js
@@ -0,0 +1,17 @@
+angular
+    .module("navigation", [])
+    .directive("twNavigation", ["$location", function($location) {
+        return {
+            "restrict": "A",
+            link: function($scope, elm, attrs) {
+                var elementLocation = elm.attr("href").substr(1);
+                $scope.$watch(function() {
+                return $location.path();
+                }, function() {
+                    if(elementLocation === $location.path()) elm.closest("li").addClass("active");
+                    else elm.closest("li").removeClass("active");
+                });
+            }
+        };
+    }])
+;
