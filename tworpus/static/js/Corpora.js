fe4e78fc93e8a07f73d5ab637b77cee5f241d454
Noodlewood <daniel.riedmuller@gmail.com>
2014-03-25 15:21:43 +0100

    corpora nav activated

---
+++ tworpus/static/js/Corpora.js
@@ -0,0 +1,7 @@
+tworpusApp
+
+    .controller("CorporaController",["$scope", "corpusCreations", function($scope, corpusCreations){
+        $scope.corpusCreations = corpusCreations.corpusCreationProcesses;
+        corpusCreations.fetchAll();
+
+}])
